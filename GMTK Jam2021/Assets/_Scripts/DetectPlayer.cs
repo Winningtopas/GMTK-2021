1076455d90c67b6c04aa70c8777c4dff441f099e
Winningtopas <jay07@live.nl>
2021-06-12 19:17:49 +0200

    Made gummy bear behaviour

---
+++ GMTK Jam2021/Assets/_Scripts/DetectPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour
+{
+
+    public Transform target = null;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") target = other.transform;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player") target = null;
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (target == null) return;
+    //}
+}
---
+++ GMTK Jam2021/Assets/_Scripts/SmallGuy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallGuy : MonoBehaviour
+{
+    private Transform objectToDodge;
+
+    [SerializeField]
+    private bool runningAway;
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private DetectPlayer detectPlayer;
+    [SerializeField]
+    private bool greenCandy = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RunFromObject();
+    }
+
+    private void RunFromObject()
+    {
+        if (detectPlayer.target != null)
+        {
+            if (!greenCandy)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, detectPlayer.target.position, -speed * Time.deltaTime);
+                runningAway = true;
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, detectPlayer.target.position, speed * Time.deltaTime);
+                runningAway = true;
+            }
+        }
+        else
+        {
+            runningAway = false;
+        }
+    }
+}
