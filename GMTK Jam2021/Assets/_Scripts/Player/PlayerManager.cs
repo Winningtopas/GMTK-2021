959607416afb016dcfb741bee557cff5e335af93
Winningtopas <jay07@live.nl>
2021-06-12 11:13:11 +0200

    Added the main mechanics (becoming bigger and then rolling around)

---
+++ GMTK Jam2021/Assets/_Scripts/Player/PlayerManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField]
+    private float sizeincrease = 0.1f;
+
+    [SerializeField]
+    private float maxSize = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreasePlayerSize(int magnitude)
+    {
+        if(transform.localScale.x < maxSize)
+        {
+            float currentSizeIncrease = sizeincrease * magnitude;
+            transform.localScale = transform.localScale + new Vector3(currentSizeIncrease, currentSizeIncrease, currentSizeIncrease);
+        }
+    }
+}
---
+++ GMTK Jam2021/Assets/_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    [SerializeField]
+    private float currentSpeed = 6f;
+    [SerializeField]
+    private float turnSmoothTime = 0.1f;
+    private float turnSmoothvelocity;
+
+    [SerializeField]
+    private Transform cam;
+    private Rigidbody rb;
+
+    public bool ballMode = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (ballMode)
+        {
+            BallMode();
+        }
+        else
+        {
+            WalkMode();
+        }
+    }
+
+    public void AcitivateBallMode()
+    {
+        ballMode = true;
+    }
+
+    private void WalkMode()
+    {
+        rb.isKinematic = true;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothvelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * currentSpeed * Time.deltaTime);
+        }
+    }
+
+    private void BallMode()
+    {
+        rb.isKinematic = false;
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            //    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothvelocity, turnSmoothTime);
+
+            //    transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            //controller.Move(moveDirection.normalized * currentSpeed * Time.deltaTime);
+            //    Debug.Log(moveDirection.normalized * currentSpeed * Time.deltaTime);
+            rb.AddForce(moveDirection.normalized * currentSpeed * 200f * Time.deltaTime);
+        }
+    }
+}
